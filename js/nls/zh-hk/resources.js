170649b986a15514a8cfa4c9221e1131fe9eeb6b
Kelly Hutchins <khutchins@esri.com>
2015-07-07 12:52:47 -0700

    Version update

---
+++ js/nls/zh-hk/resources.js
@@ -0,0 +1,19 @@
+﻿define(
+   ({
+  viewer:{
+    main:{
+      scaleBarUnits: "英語", //"english (for miles) or "metric" (for km) - don't translate.
+      legendLabel: "圖例",
+      descriptionLabel: "描述"
+    },
+    errors:{
+      createMap: "無法建立地圖",
+      general: "錯誤"
+    },
+    footer:{
+    	label: "地圖來自:"
+    }
+  }
+})
+);
+
